4c62b89e7828dff07a9ed858c9bba59d081e1f74
MrTuzzo <khirulislam5750@gmail.com>
2026-08-01 17:04:58 +0600

    admin navigation updated

--- Hdoomi/settings.py
+++ Hdoomi/settings.py
@@ -305,56 +305,23 @@ UNFOLD = {
                         "link": reverse_lazy("admin:index"),
                     },
                     {
-                        "title": _("Settings"),
-                        "icon": "settings",
-                        "link": reverse_lazy("admin:core_sitesettings_change"),
-                        "permission": _perm("core.view_sitesettings"),
-                    },
-                ],
-            },
-            {
-                "title": _("Accounts"),
-                "separator": True,
-                "collapsible": False,
-                "items": [
-                    {
                         "title": _("Users"),
                         "icon": "person",
                         "link": reverse_lazy("admin:accounts_user_changelist"),
                         "permission": _perm("accounts.view_user"),
                     },
                     {
-                        "title": _("OTP Codes"),
-                        "icon": "pin",
-                        "link": reverse_lazy("admin:accounts_otp_changelist"),
-                        "permission": _perm("accounts.view_otp"),
-                    },
-                    {
-                        "title": _("Password Reset Tokens"),
-                        "icon": "key",
-                        "link": reverse_lazy("admin:accounts_passwordresettoken_changelist"),
-                        "permission": _perm("accounts.view_passwordresettoken"),
-                    },
-                ],
-            },
-            {
-                "title": _("Wardrobe"),
-                "separator": True,
-                "collapsible": False,
-                "items": [
-                    {
                         "title": _("Categories"),
                         "icon": "checkroom",
                         "link": reverse_lazy("admin:wardrobe_category_changelist"),
                         "permission": _perm("wardrobe.view_category"),
                     },
-                ],
-            },
-            {
-                "title": _("Style News"),
-                "separator": True,
-                "collapsible": False,
-                "items": [
+                    {
+                        "title": _("Settings"),
+                        "icon": "settings",
+                        "link": reverse_lazy("admin:core_sitesettings_change"),
+                        "permission": _perm("core.view_sitesettings"),
+                    },
                     {
                         "title": _("Posts"),
                         "icon": "article",
@@ -373,6 +340,18 @@ UNFOLD = {
                         "icon": "group",
                         "link": reverse_lazy("admin:auth_group_changelist"),
                         "permission": _perm("auth.view_group"),
+                    },
+                    {
+                        "title": _("OTP Codes"),
+                        "icon": "pin",
+                        "link": reverse_lazy("admin:accounts_otp_changelist"),
+                        "permission": _perm("accounts.view_otp"),
+                    },
+                    {
+                        "title": _("Password Reset Tokens"),
+                        "icon": "key",
+                        "link": reverse_lazy("admin:accounts_passwordresettoken_changelist"),
+                        "permission": _perm("accounts.view_passwordresettoken"),
                     },
                 ],
             },
