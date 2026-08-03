031e35cb08b55a6cabd5657327295a0d601065ab
Tanjum Amin <tanjum@example.com>
2026-08-03 16:58:41 +0600

    Wardrobe item classifications: Hdoomi/urls.py

--- Hdoomi/urls.py
+++ Hdoomi/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('api/v1/wardrobe/', include('wardrobe.urls')),
     path('api/v1/news/', include('news.urls')),
     path('api/v1/avatars/', include('avatars.urls')),
+    path('api/v1/wardrobe-items-ai/', include('wardrobe_items_ai.urls')),
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
