50fc323e14c7fbfd62ad67907584991d05220a5c
Tanjum Amin <tanjum@example.com>
2026-08-03 16:58:42 +0600

    Wardrobe item classifications: wardrobe_items_ai/apps.py

---
+++ wardrobe_items_ai/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WardrobeItemsAiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "wardrobe_items_ai"
+    verbose_name = "Wardrobe Items AI"
