58a7492d2d529d22ff2c0291931d6dc295669b2c
Tanjum Amin <tanjum@example.com>
2026-08-19 12:51:08 +0600

    Saved added: avatars/urls.py

--- avatars/urls.py
+++ avatars/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path
 
-from .views import AvatarCreateView, AvatarDefaultView, AvatarListView, AvatarStatusView
+from .views import (
+    AvatarCreateView,
+    AvatarDefaultView,
+    AvatarListView,
+    AvatarSaveView,
+    AvatarStatusView,
+    SystemDefaultAvatarAdminView,
+)
 
 app_name = "avatars"
 
 urlpatterns = [
     path("", AvatarCreateView.as_view(), name="avatar-create"),
     path("default/", AvatarDefaultView.as_view(), name="avatar-default"),
+    path("admin/defaults/", SystemDefaultAvatarAdminView.as_view(), name="avatar-admin-defaults"),
     path("mine/", AvatarListView.as_view(), name="avatar-list"),
     path("<int:pk>/status/", AvatarStatusView.as_view(), name="avatar-status"),
+    path("<int:pk>/save/", AvatarSaveView.as_view(), name="avatar-save"),
 ]
 
