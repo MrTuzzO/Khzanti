17d28e33ce217b1a905376bc01e1c8e4fabf73f6
MrTuzzo <khirulislam5750@gmail.com>
2026-09-05 15:09:03 +0600

    Brand Identity updated

--- Khzanti/wsgi.py
+++ Khzanti/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Hdoomi project.
+WSGI config for Khzanti project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hdoomi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Khzanti.settings')
 
 application = get_wsgi_application()
